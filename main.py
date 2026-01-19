c51792854be9ef59bea415981b7931bf5bda0b15
cuteu <cuteuta222@gmail.com>
2026-01-19 23:59:05 +0900

    タスク ID と DELETE 機能を追加

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
+# 仮のデータ保存場所
 tasks = []
+next_id = 1
+
+
+class TaskCreate(BaseModel):
+    title: str
 
 
 class Task(BaseModel):
-  title: str
+    id: int
+    title: str
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello FastAPI"}
 
-@app.get("/hello")
-def say_hello():
-    return {"message": "こんにちは"}
-  
+
 @app.get("/tasks")
 def get_tasks():
-  return {"tasks": tasks}
+    return {"tasks": tasks}
+
 
 @app.post("/tasks")
-def create_task(task: Task):
-  tasks.append(task)
-  return {"message": "task added", "task": task}
+def create_task(task: TaskCreate):
+    global next_id
+    new_task = {
+        "id": next_id,
+        "title": task.title
+    }
+    tasks.append(new_task)
+    next_id += 1
+    return new_task
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return {"message": "task deleted"}
+    raise HTTPException(status_code=404, detail="Task not found")
